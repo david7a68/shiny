0f574687c80c989c61db4911e9afad3784f17faa
Straivers <straivers98@gmail.com>
2022-03-27 17:47:01 -0400

    Get single intersection working [js]

--- js-test/bezier.js
+++ js-test/bezier.js
@@ -2,6 +2,10 @@
 
 let canvas_size = 400;
 
+function lerp(t, x, y) {
+    return (1 - t) * x + t * y;
+}
+
 function draw_point(canvas, point, color) {
     canvas.beginPath();
     canvas.arc(point[0], canvas_size - point[1], 5, 0, 2 * Math.PI, false);
@@ -40,10 +44,6 @@ class Line {
 
     y_at(x) {
         return -(this.a * x + this.c) / this.b;
-    }
-
-    log() {
-        console.log(`y = ${-this.a / this.b} * x + ${-this.c / this.b}`);
     }
 
     x_intercept() {
@@ -91,7 +91,7 @@ class Bezier {
     }
 
     point_at(t) {
-        let ti = 1-t;
+        let ti = 1 - t;
         let x = (Math.pow(ti, 3) * this.p0[0]) + (3 * Math.pow(ti, 2) * t * this.p1[0]) + (3 * ti * Math.pow(t, 2) * this.p2[0]) + (Math.pow(t, 3) * this.p3[0]);
         let y = (Math.pow(ti, 3) * this.p0[1]) + (3 * Math.pow(ti, 2) * t * this.p1[1]) + (3 * ti * Math.pow(t, 2) * this.p2[1]) + (Math.pow(t, 3) * this.p3[1]);
         return [x, y];
@@ -118,6 +118,40 @@ class Bezier {
         canvas.stroke();
     }
 
+    split_at(t) {
+        // 1st set
+        let x1 = lerp(t, this.p0[0], this.p1[0]);
+        let y1 = lerp(t, this.p0[1], this.p1[1]);
+
+        let x2 = lerp(t, this.p1[0], this.p2[0]);
+        let y2 = lerp(t, this.p1[1], this.p2[1]);
+
+        let x3 = lerp(t, this.p2[0], this.p3[0]);
+        let y3 = lerp(t, this.p2[1], this.p3[1]);
+
+        // 2nd set
+        let x4 = lerp(t, x1, x2);
+        let y4 = lerp(t, y1, y2);
+
+        let x5 = lerp(t, x2, x3);
+        let y5 = lerp(t, y2, y3);
+
+        // 3rd set
+        let x6 = lerp(t, x4, x5);
+        let y6 = lerp(t, y4, y5);
+
+        let a = new Bezier([this.p0[0], this.p0[1]], [x1, y1], [x4, y4], [x6, y6]);
+        let b = new Bezier([x6, y6], [x5, y5], [x3, y3], [this.p3[0], this.p3[1]]);
+
+        return [a, b];
+    }
+
+    split_2(low, high) {
+        let l = this.split_at(low);
+        let h = l[1].split_at((high-low)/(1-low));
+        return [l[0], h[0], h[1]];
+    }
+
     /**
      * Returns: [thin, min, max]
      */
@@ -132,11 +166,17 @@ class Bezier {
         return [thin_line, min_line, max_line];
     }
 
+    fat_line_in_range(start, end) {
+        let thin_line = line_between(this.point_at(start), this.point_at(end));
+
+        //
+    }
+
     clip_against(line) {
         let e0 = [0, line.distance_to(this.p0)];
-        let e1 = [1/3, line.distance_to(this.p1)];
-        let e2 = [2/3, line.distance_to(this.p2)];
-        let e3 = [3/3, line.distance_to(this.p3)];
+        let e1 = [1 / 3, line.distance_to(this.p1)];
+        let e2 = [2 / 3, line.distance_to(this.p2)];
+        let e3 = [3 / 3, line.distance_to(this.p3)];
 
         if (e0[1] < 0 && e1[1] < 0 && e2[1] < 0 && e3[1] < 0) {
             return [0, 0];
@@ -151,7 +191,6 @@ class Bezier {
             let x2 = l2.x_intercept();
             let x3 = l3.x_intercept();
 
-            // min above 0
             var min = 1000;
             if (x1 > 0 && x1 < min) {
                 min = x1;
@@ -189,6 +228,43 @@ class Bezier {
 
         return [low, high];
     }
+
+    find_intersections(other) {
+        var a = this;
+        var a_clip = [0, 1];
+        var b = other;
+        var b_clip = [0, 1];
+        var iteration = 0;
+
+        while (Math.abs(a_clip[0] - a_clip[1]) > 0.00001) {
+            console.log(a_clip, " : ", b_clip);
+            console.log(Math.abs(a_clip[0] - a_clip[1]), " : ", Math.abs(b_clip[0] - b_clip[1]))
+            if (iteration % 2 == 0) {
+                let lines = a.get_fat_line();
+                let low = b.clip_against(lines[1].negate());
+                let high = b.clip_against(lines[2]);
+                let t_start = Math.max(low[0], high[0]);
+                let t_end = Math.min(low[1], high[1]);
+
+                b_clip = [t_start, t_end];
+                b = b.split_2(t_start, t_end)[1];
+            } else {
+                let lines = b.get_fat_line();
+                let low = a.clip_against(lines[1].negate());
+                let high = a.clip_against(lines[2]);
+                let t_start = Math.max(low[0], high[0]);
+                let t_end = Math.min(low[1], high[1]);
+
+                a_clip = [t_start, t_end];
+                a = a.split_2(t_start, t_end)[1];
+            }
+
+            iteration++;
+        }
+
+        console.log(iteration);
+        return [a_clip[0]];
+    }
 }
 
 let samples = [
@@ -196,14 +272,14 @@ let samples = [
         new Bezier([18, 122], [15, 178], [247, 173], [251, 242]),
         new Bezier([24, 21], [189, 40], [159, 137], [101, 261])
     ),
-    new Sample(
-        new Bezier([204, 41], [45, 235], [220, 235], [226, 146]),
-        new Bezier([100, 98], [164, 45], [187, 98], [119, 247])
-    ),
-    new Sample(
-        new Bezier([18, 122], [15, 178], [247, 173], [251, 242]),
-        new Bezier([20, 213], [189, 40], [85, 283], [271, 217])
-    )
+    // new Sample(
+    //     new Bezier([204, 41], [45, 235], [220, 235], [226, 146]),
+    //     new Bezier([100, 98], [164, 45], [187, 98], [119, 247])
+    // ),
+    // new Sample(
+    //     new Bezier([18, 122], [15, 178], [247, 173], [251, 242]),
+    //     new Bezier([20, 213], [189, 40], [85, 283], [271, 217])
+    // )
 ];
 
 function step1_original(sample, canvas) {
@@ -228,17 +304,34 @@ function step3_thick_line(sample, canvas) {
 }
 
 function step4_clip(sample, canvas) {
-    sample.draw(canvas, false);
+    // sample.draw(canvas, true);
 
     let lines = sample.curve1.get_fat_line();
+    lines[0].draw(canvas, 0, canvas_size, 'lightgrey');
+    lines[1].draw(canvas, 0, canvas_size, 'lightgrey');
+    lines[2].draw(canvas, 0, canvas_size, 'lightgrey');
+
     let clip_low = sample.curve2.clip_against(lines[1].negate());
     let clip_high = sample.curve2.clip_against(lines[2]);
 
     let t_start = Math.max(clip_low[0], clip_high[0]);
     let t_end = Math.min(clip_low[1], clip_high[1]);
 
-    draw_point(canvas, sample.curve2.point_at(t_start), 'green');
-    draw_point(canvas, sample.curve2.point_at(t_end), 'purple');
+    let clipped = sample.curve2.split_2(t_start, t_end);
+    sample.curve1.draw(canvas, 'red');
+    clipped[0].draw(canvas, 'green');
+    clipped[1].draw(canvas, 'blue');
+    clipped[2].draw(canvas, 'green');
+}
+
+function step5_intersect(sample, canvas) {
+    sample.draw(canvas, false);
+
+    let intersections = sample.curve1.find_intersections(sample.curve2);
+
+    intersections.forEach(t => {
+        draw_point(canvas, sample.curve1.point_at(t));
+    });
 }
 
 let steps = [
@@ -246,6 +339,7 @@ let steps = [
     [step2_thin_line, "Thin Line"],
     [step3_thick_line, "Thick Line"],
     [step4_clip, "Clip Bezier"],
+    [step5_intersect, "Find Intersections"]
 ];
 
 function run() {
