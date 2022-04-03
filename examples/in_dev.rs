c9dd691b87b82410397891a8d430434668bea1c6
Straivers <straivers98@gmail.com>
2022-04-03 03:22:06 -0400

    Add WIP runner 'example'
    File used for testing work in progress.

--- examples/in_dev.rs
+++ examples/in_dev.rs
@@ -6,71 +6,70 @@ use shiny::{
     color::{Color, Rgb, Srgb8},
     image::{cpu_image::CpuImage, Image},
     shapes::{
-        bezier::{intersections, Bezier, CubicSlice},
-        path::Builder,
+        bezier::{Bezier, Cubic, CubicSlice},
         point::Point,
     },
 };
 
 fn main() {
-    let mut image = CpuImage::new(300, 300);
-
-    let path1 = {
-        let mut builder = Builder::new(Point::new(24.0, 21.0));
-        builder.add_cubic(
-            Point::new(189.0, 40.0),
-            Point::new(159.0, 137.0),
-            Point::new(101.0, 261.0),
-        );
-        builder.build()
-    };
-    let curve1 = path1.iter().next().unwrap().next().unwrap();
-
-    let path2 = {
-        let mut builder = Builder::new(Point::new(18.0, 122.0));
-        builder.add_cubic(
+    let curve1 = Cubic {
+        points: [
+            Point::new(18.0, 122.0),
             Point::new(15.0, 178.0),
             Point::new(247.0, 173.0),
             Point::new(251.0, 242.0),
-        );
-        builder.build()
+        ],
     };
-    let curve2 = path2.iter().next().unwrap().next().unwrap();
 
+    let curve2 = Cubic {
+        points: [
+            Point::new(20.0, 213.0),
+            Point::new(189.0, 40.0),
+            Point::new(85.0, 283.0),
+            Point::new(271.0, 217.0),
+        ],
+    };
+
+    let mut image = CpuImage::new(300, 300);
     draw_curve(
-        &curve1,
+        curve1.borrow(),
         0.0,
         1.0,
         Srgb8 {
-            color: Rgb { r: 255, g: 0, b: 0 },
+            color: Rgb {
+                r: 100,
+                g: 100,
+                b: 100,
+            },
         },
         &mut image,
     );
 
     draw_curve(
-        &curve2,
+        curve2.borrow(),
         0.0,
         1.0,
         Srgb8 {
-            color: Rgb { r: 0, g: 255, b: 0 },
+            color: Rgb {
+                r: 100,
+                g: 100,
+                b: 100,
+            },
         },
         &mut image,
     );
 
-    let intersections = intersections(&curve1.points, &curve2.points);
-    println!("{:?}", &intersections.0);
+    let intersections = curve1.intersections(&curve2);
+    println!("{:?}", intersections);
 
-    for t in intersections.0.iter() {
-        let points = curve1.at(*t);
+    for t in intersections.iter() {
+        let intersection = curve1.at(t.0);
+        println!("{:?}", intersection);
         image.set(
-            points.x.round() as u32,
-            points.y.round() as u32,
+            intersection.x.round() as u32,
+            intersection.y.round() as u32,
             Srgb8 {
-                color: Rgb {
-                    r: 255,
-                    g: 255,
-                    b: 255,
-                },
+                color: Rgb { r: 255, g: 0, b: 0 },
             },
         );
     }
@@ -78,7 +77,7 @@ fn main() {
     write_png(image.get_pixels(), module_path!());
 }
 
-fn draw_curve<C: Color>(curve: &CubicSlice, from: f32, to: f32, color: C, image: &mut CpuImage<C>) {
+fn draw_curve<C: Color>(curve: CubicSlice, from: f32, to: f32, color: C, image: &mut CpuImage<C>) {
     let mut t = from;
     let d = 0.001;
     loop {
